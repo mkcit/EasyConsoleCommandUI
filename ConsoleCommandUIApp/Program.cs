d43152f14060dd7f42ff05983015eb1b2b886f2d
mks.cit <mks.cit@outlook.com>
2024-10-27 21:12:46 +0200

    Some new updates

--- ConsoleCommandUIApp/Program.cs
+++ ConsoleCommandUIApp/Program.cs
@@ -10,13 +10,17 @@ namespace ConsoleCommandUIApp
 				new Command("SUM TWO VALUES [+]"),
 				new Command("SUB TWO VALUES [-]"),
 				new Command("MUL TWO VALUES [×]"),
-				new Command("DIV TOW VALUES [/]")
+				new Command("DIV TOW VALUES [/]"),
+				new Command("END THE SYSTEM [ESC]")
 			};
 
-			ConsoleCommandUI consoleCommandUI = 
-				new ConsoleCommandUI(commands);
+			ConsoleCommandUI consoleCommandUI =
+				new ConsoleCommandUI(commands,
+				padding: 3);
 
 			consoleCommandUI.CommandPressed += ConsoleCommandUI_CommandPressed;
+
+			
 
 			consoleCommandUI.ShowOnScreen();
 		}
@@ -24,7 +28,16 @@ namespace ConsoleCommandUIApp
 		private static void ConsoleCommandUI_CommandPressed(
 			ConsoleCommandUI source, ConsoleKey key, int commandIndex)
 		{
-			
+			if (key == ConsoleKey.Escape || commandIndex == source.Commands.Length - 1)
+			{
+				source.End = true;
+			}
+			else if (key == ConsoleKey.Enter)
+			{
+				switch (commandIndex)
+				{
+				}
+			}
 		}
 	}
 }
