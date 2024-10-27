226040391bbb83edd3f5501a2787eba2bdeee523
mks.cit <mks.cit@outlook.com>
2024-10-27 21:08:19 +0200

    set namespace

--- ConsoleCommandUIApp/Program.cs
+++ ConsoleCommandUIApp/Program.cs
@@ -1,25 +1,28 @@
-﻿namespace ConsoleCommandUIApp
+﻿using CLConsoleCommandUI;
+
+namespace ConsoleCommandUIApp
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			CLConsoleCommandUI.Command[] commands = {
-				new CLConsoleCommandUI.Command("SUM TWO VALUES [+]"),
-				new CLConsoleCommandUI.Command("SUB TWO VALUES [-]"),
-				new CLConsoleCommandUI.Command("MUL TWO VALUES [×]"),
-				new CLConsoleCommandUI.Command("DIV TOW VALUES [/]")
+			Command[] commands = {
+				new Command("SUM TWO VALUES [+]"),
+				new Command("SUB TWO VALUES [-]"),
+				new Command("MUL TWO VALUES [×]"),
+				new Command("DIV TOW VALUES [/]")
 			};
 
-			CLConsoleCommandUI.ConsoleCommandUI consoleCommandUI = 
-				new CLConsoleCommandUI.ConsoleCommandUI(commands);
+			ConsoleCommandUI consoleCommandUI = 
+				new ConsoleCommandUI(commands);
 
 			consoleCommandUI.CommandPressed += ConsoleCommandUI_CommandPressed;
 
 			consoleCommandUI.ShowOnScreen();
 		}
 
-		private static void ConsoleCommandUI_CommandPressed(CLConsoleCommandUI.ConsoleCommandUI source, ConsoleKey key, int commandIndex)
+		private static void ConsoleCommandUI_CommandPressed(
+			ConsoleCommandUI source, ConsoleKey key, int commandIndex)
 		{
 			
 		}
